96bad4197a1ce8507c52a8a25e241da5a88acf7c
CristelAlm <cristelkam@gmail.com>
2021-01-08 10:54:04 -0600

    Version 22

---
+++ vistas/inicio.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+
+	if(isset($_SESSION['usuario'])){
+		include "header.php";
+?>
+
+
+
+
+<?php 
+		include "footer.php";
+	} else {
+		header("location:../index.php");
+	}
+?>
